33d36863e0c3c1e30b3c883637519b0c8fd7c54d
Elkhan <elxanmursal@gmail.com>
2020-02-02 13:33:14 +0100

    Clean up. Tiny fix

--- Tic-Tac-Toe/src/lib/game-logic.js
+++ Tic-Tac-Toe/src/lib/game-logic.js
@@ -1,61 +1,9 @@
-import { tripleEqual } from "./util";
-
 export function fieldIsEmpty(board, xCoordinate, yCoordinate) {
   return !board[xCoordinate][yCoordinate];
 }
 
-export function findWinnerOld(board) {
-  for (let i = 0; i < 3; i++) {
-    // columns
-    if (board[0][i] && tripleEqual(board[0][i], board[1][i], board[2][i])) {
-      return board[0][i];
-    }
-    // rows
-    if (board[i][0] && tripleEqual(board[i][0], board[i][1], board[i][2])) {
-      return board[i][0];
-    }
-  }
-  // diagonals
-  if (board[0][0] && tripleEqual(board[0][0], board[1][1], board[2][2])) {
-    return board[0][0];
-  }
-  if (board[0][2] && tripleEqual(board[0][2], board[1][1], board[2][0])) {
-    return board[0][2];
-  }
-
-  return false;
-}
-
 export function findWinner(board) {
-  // Horizontal check
-  for (let row = 0; row < board.length; row++) {
-    const player = board[row][0];
-    for (let col = 1; col < board.length; col++) {
-      if (board[row][col] !== player) {
-        break;
-      }
-
-      if (col === board.length - 1) {
-        return player;
-      }
-    }
-  }
-
-  // Vertical check
-  for (let col = 0; col < board.length; col++) {
-    const player = board[0][col];
-    for (let row = 1; row < board.length; row++) {
-      if (board[row][col] !== player) {
-        break;
-      }
-
-      if (row === board.length - 1) {
-        return player;
-      }
-    }
-  }
-
-  // Diagonal check
+  // Diagonal check first because of less time complexity
   // Left to right
   let player = board[0][0];
   for (
@@ -85,6 +33,34 @@ export function findWinner(board) {
 
     if (row === board.length - 1 && col === 0) {
       return player;
+    }
+  }
+
+  // Horizontal check
+  for (let row = 0; row < board.length; row++) {
+    player = board[row][0];
+    for (let col = 1; col < board.length; col++) {
+      if (board[row][col] !== player) {
+        break;
+      }
+
+      if (col === board.length - 1) {
+        return player;
+      }
+    }
+  }
+
+  // Vertical check
+  for (let col = 0; col < board.length; col++) {
+    player = board[0][col];
+    for (let row = 1; row < board.length; row++) {
+      if (board[row][col] !== player) {
+        break;
+      }
+
+      if (row === board.length - 1) {
+        return player;
+      }
     }
   }
 

f10ef4195fb12d5b3a93117bffe12cd85ba98fa7
Elkhan <elxanmursal@gmail.com>
2020-02-11 21:53:22 +0100

    Refactoring input validation. More user friendly

--- src/Components/GameSizer.js
+++ src/Components/GameSizer.js
@@ -1,23 +1,27 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import "./GameSizer.css";
 
-const GameSizer = props => {
-  const [size, setSize] = useState("");
+const GameSizer = ({ onStartGame }) => {
+  const [size, setSize] = useState({ value: "", touched: false });
+  const [message, setMessage] = useState("");
+
+  useEffect(() => {
+    if (!size.value && size.touched) {
+      setMessage("Please, specify the size of game.");
+    } else if (size.value < 3 && size.touched) {
+      setMessage("Game doesn't make sense if the size is less than 3.");
+      setSize("");
+    } else if (size.value >= 3) {
+      setMessage("");
+    }
+  }, [size]);
 
   const startGame = () => {
-    if (!size) {
-      alert("Please, specify the size of game.");
-      return;
-    } else if (size < 3) {
-      alert("Game doesn't make sense if the size is less than 3.");
-      setSize("");
-      return;
-    }
-
-    props.onStartGame(size);
+    onStartGame(size.value);
   };
 
   const sizeChangeHandler = event => {
-    setSize(event.target.value);
+    setSize({ value: event.target.value, touched: true });
   };
 
   return (
@@ -26,10 +30,13 @@ const GameSizer = props => {
         type="number"
         placeholder="size (e.g. 3 for 3x3, 5 for 5x5) etc."
         style={{ width: "200px" }}
-        value={size}
+        value={size.value}
         onChange={sizeChangeHandler}
       />
-      <button onClick={startGame}>Start game</button>
+      <button onClick={startGame} disabled={!size.value || size.value < 3}>
+        Start game
+      </button>
+      {message ? <p>{message}</p> : ""}
     </div>
   );
 };
